510b04d58cd7c1deefddb3e879e6119a41e0e309
John Mirschel <evan.mirschel@gmail.com>
2021-05-31 13:29:50 -0700

    Adding postgres integration

--- manage.py
+++ manage.py
@@ -1,9 +1,18 @@
 # manage.py
 from flask.cli import FlaskGroup
-from src import app
+from src import app, db
 
 
 cli = FlaskGroup(app)
+
+@cli.command('recreate-db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+
 
 
 if __name__ == '__main__':
